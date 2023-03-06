176ea499c61b2659e3d37d723cf235c6cfeb0f3a
Rahul Kumar <rahulkumar@skyeair.tech>
2023-03-06 22:45:01 +0530

    pro-khelo project

---
+++ backend/src/Routes/Routes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const userController = require("../Controllers/UserController")
+const productController = require("../Controllers/ProductController")
+const middleWare = require("../Middleware/Middleware")
+
+
+router.post("/register",userController.createUser)
+router.post("/login",userController.loginUser)
+router.get("/profileDetail",middleWare.authentication,userController.profileDetail)
+router.put("/update-user/:userId",middleWare.authentication,middleWare.authorisation,userController.updateProfile)
+router.post("/add-product",middleWare.authentication,productController.createProduct)
+router.delete("/delete-product/:productId",middleWare.authentication,middleWare.authorisation,productController.deleteProduct)
+router.get("/get-product",productController.getProduct)
+router.post("/aggregation",productController.mongoDbAggregation)
+router.get("/profiles",userController.profileDetails)
+router.get("/get-product/:productId",productController.getProductById)
+router.put("/update-product/:productId",middleWare.authentication,middleWare.authorisation,productController.updateProduct)
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
